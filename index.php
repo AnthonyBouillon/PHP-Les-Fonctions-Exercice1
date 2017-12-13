eb198c0db39df628f6d41f3aaf6140c0df6c41a8
Anthony <anthony_bouillon@outlook.fr>
2017-12-13 09:15:08 +0100

    init commit from bash for PHP-Les-Fonctions-Exercice1

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Les fonctions Exercice 1</title>
+  </head>
+  <body>
+<?php
+  function callTrue(){
+    return true;
+  };
+  echo callTrue();
+ ?>
+  </body>
+</html>
